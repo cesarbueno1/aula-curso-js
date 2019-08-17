b38c3d9dfbeb82a216d6e675f5f9315da54eaa55
cesarbueno <unzerb@yahoo,com>
2019-08-17 15:01:33 -0300

    meu primeiro commit

---
+++ aula1.js
@@ -0,0 +1,31 @@
+console.log("nada de olá mundo hj")
+console.warn("eu sou um alerta de erro de erro - cuidado!")
+alert("agora eu programo javascript")
+var idade = 12
+var nome = "Cesar"
+var salario = 980.99
+
+var pais = ["Rita","Celso"]
+var veiculo = {
+    tipo: "motocicleta",
+    ano: 2012,
+    marca: "honda",
+    valor: 1000.99,
+    motor: {
+        cilindros: 4,
+        valvulas:16
+    }
+
+}
+var clientes = [
+    {
+        nome 
+     }]
+var relatorio = nome + " tem " + idade + " anos e recebe " + salario + " filho de " + pais[1] + " e " + pais[0]
+console.log(idade)
+console.log(nome)
+console.log(relatorio)
+console.log(salario)
+console.log(pais)
+console.log(pais.length)
+console.log(veiculo)
